c0af07f71f438b766aba8a2026f92064145573a7
hameederrawi <hameed@medsuk.co.uk>
2020-06-02 03:58:58 +0000

    adding next and prev button

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const baseURL =  "https://ci-swapi.herokuapp.com/api/";
 
-function getData(type, cb) {
+function getData(url, cb) {
 var xhr = new XMLHttpRequest();
 
-xhr.open("GET", baseURL + type + "/");
+xhr.open("GET", url);
 xhr.send();
 
 xhr.onreadystatechange = function() {
@@ -11,8 +10,7 @@ xhr.onreadystatechange = function() {
        cb(JSON.parse(this.responseText));
     }
 };
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
+    
 }
 
 function getTableHeaders(obj) {
@@ -24,12 +22,28 @@ function getTableHeaders(obj) {
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
-    var el = document.getElementById("data");
-    el.innerHTML = "";
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    }  else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
 
-    getData(type, function(data) {
-        var tableRows = [];
+function writeToDocument(url) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    
+
+    getData(url, function(data) {
+
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous)
+        }
+        
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
@@ -44,7 +58,7 @@ function writeToDocument(type) {
            tableRows.push(`<tr${dataRow}></tr>`);
            
         });
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }   
 
